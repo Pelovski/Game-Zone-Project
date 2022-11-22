c391b84d2df714c181cc408c29989dc5b83a7978
Pavel Pelovski <git.pelovski@gmail.com>
2022-11-22 18:28:10 +0100

    Integrated Firebase

--- GameZone/src/environments/environment.prod.ts
+++ GameZone/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase:{
+    apiKey: "AIzaSyBnNH3FyVwEfyhMucL-ZE-3xwfmTiG9TTc",
+    authDomain: "gamezone-1839a.firebaseapp.com",
+    projectId: "gamezone-1839a",
+    storageBucket: "gamezone-1839a.appspot.com",
+    appId: "1:288458228309:web:f590750d1f2e41289ccadc"
+  }
 };
--- GameZone/src/environments/environment.ts
+++ GameZone/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase:{
+    apiKey: "AIzaSyBnNH3FyVwEfyhMucL-ZE-3xwfmTiG9TTc",
+    authDomain: "gamezone-1839a.firebaseapp.com",
+    projectId: "gamezone-1839a",
+    storageBucket: "gamezone-1839a.appspot.com",
+    appId: "1:288458228309:web:f590750d1f2e41289ccadc"
+  }
 };
 
 /*
