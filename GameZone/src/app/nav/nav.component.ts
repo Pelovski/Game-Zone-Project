5379b80318d4d8ef6169ad1c19db73de09fc57ab
Pavel Pelovski <git.pelovski@gmail.com>
2022-11-28 12:19:11 +0100

    Nav accessibility changed when user is auth

--- GameZone/src/app/nav/nav.component.ts
+++ GameZone/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,15 +9,17 @@ import { ModalService } from '../services/modal.service';
 })
 export class NavComponent implements OnInit {
 
-  constructor(public modal: ModalService) { }
+  constructor(public modal: ModalService, public auth: AuthService) {
+
+  }
 
   ngOnInit(): void {
   }
 
-  openModal($event: Event){
+  openModal($event: Event) {
     $event.preventDefault();
 
-    this.modal.toggleModle('auth');    
+    this.modal.toggleModle('auth');
   }
 
 }
--- GameZone/src/app/services/auth.service.ts
+++ GameZone/src/app/services/auth.service.ts
@@ -10,37 +10,46 @@ import { map } from 'rxjs/operators'
   providedIn: 'root'
 })
 export class AuthService {
-
-  private userCollection: AngularFirestoreCollection<IUser>;
-  public isAuthenticated$: Observable<boolean>;
+  private userCollection: AngularFirestoreCollection<IUser>;  // reference - more readably and Type safety
+  public isAuthenticated$: Observable<boolean>; //Identifying properties as observable
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+    //Create a collection of users in the database
     this.userCollection = db.collection('users');
+    //Current authentication status
     this.isAuthenticated$ = auth.user.pipe(
       map(user => !!user)
     )
   }
 
+  // Create user with given data from the registerform
   public async createUser(userData:IUser){
 
+    //Cheking if a password was given
     if(!userData.password){
       throw new Error('Password not provided!');   
     }
+
+    //Firebase authenticate the user
     const userCredentions = await this.auth.createUserWithEmailAndPassword(
       userData.email as string, userData.password as string
     );
 
+    //Checking if the user property is null
     if(!userCredentions.user){
       throw new Error("User can't be found");
       
     }
 
-    await this.userCollection.doc(userCredentions.user.uid).set({
+    // Type safety
+    // If doc with given ID doesn't exist will be created and will return document
+    await this.userCollection.doc(userCredentions.user.uid).set({ 
       name: userData.name,
       email: userData.email,
       age: userData.age
     });
     
+    // Update user profile name
     await userCredentions.user.updateProfile({
       displayName: userData.name
     });
