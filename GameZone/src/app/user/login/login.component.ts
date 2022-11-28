dbe919a6058912ce5bc9c9164f1f54f108f49eda
Pavel Pelovski <git.pelovski@gmail.com>
2022-11-28 13:05:51 +0100

    Loggin auth completed

--- GameZone/src/app/user/login/login.component.ts
+++ GameZone/src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -8,17 +9,40 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   credentials = {
-    email:'',
+    email: '',
     password: ''
   };
 
-  constructor() { }
+
+  showAlert = false;
+  alertMessage = 'Please wait! We are logging you in.';
+  alertColor = 'blue';
+  inSubmission = false;
+
+  constructor(private auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }
 
-  login(){
-     
+  async login() {
+    this.showAlert = true;
+    this.alertMessage = 'Please wait! We are logging you in.';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email, this.credentials.password
+      )
+    } catch (e) {
+      this.alertMessage = 'An unexpected error occurred. Please try again later.';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+
+    this.alertMessage =  'Success! Your are now logged in.'
+    this.alertColor = 'green';
   }
 
 }
--- GameZone/src/main.ts
+++ GameZone/src/main.ts
@@ -3,10 +3,27 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth'
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+//Initialize Firebase first
+firebase.initializeApp(environment.firebase);
+
+let appInit = false;
+
+//Calling this funktion to gain access to the auth methods, with is where we can listen for the event
+//Firebase will emmit an event whenever the user's auth sate changes. (Login / Logout)
+firebase.auth().onAuthStateChanged(() => {
+
+  // Check if we have alredy Initialize Angular
+  if (!appInit) {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  }
+
+  appInit = true;
+});
