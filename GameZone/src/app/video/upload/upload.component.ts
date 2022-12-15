aa4f35f9bd35f12f1995ab4c750864f1b6d1bbbf
Pavel Pelovski <git.pelovski@gmail.com>
2022-12-15 18:14:45 +0100

    Handling errors and successful uploads

--- GameZone/src/app/video/upload/upload.component.ts
+++ GameZone/src/app/video/upload/upload.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { v4 as uuid} from 'uuid';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app'; 
 
 @Component({
   selector: 'app-upload',
@@ -9,9 +12,19 @@ import { v4 as uuid} from 'uuid';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+
   isDragover = false;
   file: File | null = null;
   nextStep = false;
+
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please wait! Your video is being uploaded.';
+  inSubmission = false;
+
+  percentage = 0;
+  showPercentage = false;
+  user: firebase.User | null = null;
 
   title =  new FormControl('',{
   validators:[
@@ -26,8 +39,8 @@ uploadForm = new FormGroup({
 title: this.title
 });
 
-  constructor(private storage: AngularFireStorage) {
-
+  constructor(private storage: AngularFireStorage , private auth: AngularFireAuth) {
+    auth.user.subscribe(user => this.user = user);
    }
 
   ngOnInit(): void {
@@ -37,8 +50,6 @@ title: this.title
     this.isDragover = false;
 
     this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
-    console.log(this.file);
-    
 
     if(!this.file || this.file.type !== 'video/mp4'){
       return;
@@ -47,17 +58,56 @@ title: this.title
     this.title.setValue(
       this.file.name.replace(/\.[^/.]+$/, '')
     );
+
     this.nextStep = true;
   }
 
   uploadFile(){
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait! Your video is being uploaded.';
+    this.inSubmission = true;
+    this.showPercentage = true;
 
-    console.log('upload file');
-    
     const videoFileName = uuid();
     const videoPath = `videos/${videoFileName}.mp4`;
 
-    this.storage.upload(videoPath, this.file);
+    const task = this.storage.upload(videoPath, this.file);
+    const videoref = this.storage.ref(videoPath);
+
+    task.percentageChanges().subscribe(progress =>{
+      this.percentage = progress as number / 100
+    });
+
+    task.snapshotChanges().pipe(
+      last(),
+      switchMap(() => videoref.getDownloadURL())
+    ).subscribe({
+      next: (url) =>{
+        const video = {
+          
+          uid: this.user?.uid,
+          displayName: this.user?.displayName,
+          title: this.title.value,
+          fileName: `${videoFileName}.mp4`,
+          url
+        };
+
+        console.log(video);
+        
+
+        this.alertColor = 'green';
+        this.alertMsg = 'Success! Your video is now ready to share with the world.';
+        this.showPercentage = false;
+      },
+      error: (error) => {
+        this.alertColor = 'red';
+        this.alertMsg = 'Upload failed! Please try again later.'
+        this.inSubmission = true;
+        this.showPercentage = false;
+        console.error(error);
+      }
+    });
   }
 
 }
