c4f10b0609873da088c29cb913c347ad95a272fc
Pavel Pelovski <git.pelovski@gmail.com>
2022-12-15 11:03:57 +0100

    Upload functionality started

--- GameZone/src/app/video/upload/upload.component.ts
+++ GameZone/src/app/video/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+  isDragover = false;
+  file: File | null = null;
+  nextStep = false;
+
+  title =  new FormControl('',{
+  validators:[
+    Validators.required,
+    Validators.minLength(3)
+  ],
+  nonNullable: true
+});
+
+uploadForm = new FormGroup({
+title: this.title
+});
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  storeFile($event: Event){
+    this.isDragover = false;
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    if(!this.file || this.file.type !== 'video/mp4'){
+      return;
+    }
+
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    );
+    this.nextStep = true;
+  }
+
+  uploadFile(){
+    
   }
 
 }
